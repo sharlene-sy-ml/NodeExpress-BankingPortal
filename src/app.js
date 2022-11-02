9de5da7a173d047b5151e05c74841a8641332de2
sakyadhita <service.sakyadhita@gmail.com>
2022-11-02 17:24:39 +0800

    Module 1

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const fs = require("fs")
+const path = require("path")
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"))
+app.set("public", path.join(__dirname, "public"))
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("index", { 'title': 'Index' })
+})
+
+app.listen(3000, () => console.log("PS Project Running on port 3000!"))
