98c4788d70878a3356677ae9768b8531f34e79b3
sakyadhita <service.sakyadhita@gmail.com>
2022-11-02 19:09:26 +0800

    Module 2

--- src/app.js
+++ src/app.js
@@ -4,12 +4,36 @@ const path = require("path")
 
 const app = express();
 
+const accountData = fs.readFileSync("src/json/accounts.json", { 'encoding': 'utf8' })
+const accounts = JSON.parse(accountData)
+
+const userData = fs.readFileSync("src/json/users.json", { 'encoding': 'utf8' })
+const users = JSON.parse(userData)
+
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
 app.use(express.static(path.join(__dirname, "public")))
+
 app.get("/", (req, res) => {
-    res.render("index", { 'title': 'Index' })
+    res.render("index", { 'title': 'Account Summary', 'accounts': accounts })
 })
+
+app.get("/savings", (req, res) => {
+    res.render("account", { 'title': 'Account Summary', 'account': accounts.savings })
+})
+
+app.get("/checking", (req, res) => {
+    res.render("account", { 'title': 'Account Summary', 'account': accounts.checking })
+})
+
+app.get("/credit", (req, res) => {
+    res.render("account", { 'title': 'Account Summary', 'account': accounts.credit })
+})
+
+app.get("/profile", (req, res) => {
+    res.render("profile", { 'user': users[0] })
+})
+
 
 app.listen(3000, () => console.log("PS Project Running on port 3000!"))
