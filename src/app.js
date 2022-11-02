cec0655937337a617fc835de97ab735d8be7eebd
sakyadhita <service.sakyadhita@gmail.com>
2022-11-02 17:35:17 +0800

    Module 1 public directory to static

--- src/app.js
+++ src/app.js
@@ -5,9 +5,9 @@ const path = require("path")
 const app = express();
 
 app.set("views", path.join(__dirname, "views"))
-app.set("public", path.join(__dirname, "public"))
 app.set("view engine", "ejs")
 
+app.use(express.static(path.join(__dirname, "public")))
 app.get("/", (req, res) => {
     res.render("index", { 'title': 'Index' })
 })
